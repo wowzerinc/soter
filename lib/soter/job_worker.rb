d5e09a2e0beff7236d11c5009331ab8630598081
Andres Freyria <andres.freyria@gmail.com>
2013-01-29 13:34:31 -0600

    Fixing indentation

--- lib/soter/job_worker.rb
+++ lib/soter/job_worker.rb
@@ -1,4 +1,4 @@
-module Soter  
+module Soter
   class JobWorker
 
     require 'digest/md5'
@@ -34,33 +34,33 @@ module Soter
         job.each {
           |key, value| log  "#{process_id}: {#{key} : #{value}}" }
 
-          begin
-            handler_class = Soter.recursive_const_get(job['job']['class'])
-            job_handler   = handler_class.new(job['job']['params'])
+        begin
+          handler_class = Soter.recursive_const_get(job['job']['class'])
+          job_handler   = handler_class.new(job['job']['params'])
 
-            job_handler.perform
+          job_handler.perform
 
-            log "#{process_id}: " + job_handler.message
+          log "#{process_id}: " + job_handler.message
 
-            if job_handler.success?
-              @queue.complete(job, process_id)
-              log "#{process_id}: Completed job #{job['_id']}"
-            else
-              offset           = Soter.retry_offset(job['attempts']+1)
-              job['active_at'] = Time.now.utc + offset 
-              @queue.error(job, job_handler.message)
+          if job_handler.success?
+            @queue.complete(job, process_id)
+            log "#{process_id}: Completed job #{job['_id']}"
+          else
+            offset           = Soter.retry_offset(job['attempts']+1)
+            job['active_at'] = Time.now.utc + offset
+            @queue.error(job, job_handler.message)
 
-              log "#{process_id}: Failed job #{job['_id']}"
-            end
-
-            sleep(1) if fork?
-          rescue Exception => e
-            @queue.complete(job, e.message)
-            log "#{process_id}: Failed job #{job['_id']}" +
-              " with error #{e.message}"
-            log "#{process_id}: Backtrace =>"
-            e.backtrace.each { |line| log "#{process_id}: #{line}"}
+            log "#{process_id}: Failed job #{job['_id']}"
           end
+
+          sleep(1) if fork?
+        rescue Exception => e
+          @queue.complete(job, e.message)
+          log "#{process_id}: Failed job #{job['_id']}" +
+            " with error #{e.message}"
+          log "#{process_id}: Backtrace =>"
+          e.backtrace.each { |line| log "#{process_id}: #{line}"}
+        end
       end #while
       log "#{process_id}: Harakiri"
       @log.close
