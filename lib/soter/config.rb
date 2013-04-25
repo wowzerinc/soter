71838ed52033ac0fdc0a9e9b36fb6dc152512278
Andres Freyria <andres.freyria@gmail.com>
2013-04-25 11:51:23 -0500

    Adding more config options + defaults

--- lib/soter/config.rb
+++ lib/soter/config.rb
@@ -1,16 +1,15 @@
 module Soter
   class Config < Struct.new(:fork, :logfile, :logger, :host, :port, :db,
-                            :workers, :attempts, :hosts)
+                            :workers, :attempts, :hosts, :timeout, :collection)
 
     def queue_settings
-      host_settings = host ? {host: host} : {hosts: hosts}
+      host_settings = host ? {host: host, port: port} : {hosts: hosts}
 
       host_settings.merge({
-        port:       port,
         database:   db,
-        collection: "soter_queue",
-        timeout:    300,
-        attempts:   (attempts || 3)
+        collection: collection || "soter_queue",
+        timeout:    timeout || 300,
+        attempts:   attempts || 3
       })
     end
 
